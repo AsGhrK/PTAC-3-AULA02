df01c32b35d055a363e3ef02f78f0c9ddcd2d219
Davi Casari <davicasari1@gmail.com>
2024-02-29 00:49:42 -0400

    ebaa 2.0 explosão bum bum

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-images:{
-    domains:[
-        "www.ifms.edu.br" ,  "ead.ifms.edu.br"
-    ]
-   
-}
-
-}
-
-module.exports = nextConfig
+  images: {
+    domains: ["www.ifms.edu.br", "ead.ifms.edu.br"],
+  },
+};
+module.exports = nextConfig;
